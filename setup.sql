aae470b0e2ea910dea05a75f9e3ea23117cce498
Bryan <bryankoh137@hotmail.com>
2019-10-25 18:26:58 +0800

    update sql

--- setup.sql
+++ setup.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS TrashBinInfo CASCADE;
 DROP TABLE IF EXISTS SensorData CASCADE;
+DROP TRIGGER IF EXISTS on_recv_sensor ON SensorData;
 
 CREATE TABLE TrashBinInfo (
 	bin_id varchar(100) PRIMARY KEY,
@@ -20,3 +21,18 @@ INSERT INTO TrashBinInfo VALUES ('bin/sensor1', 0, 'lat lang');
 INSERT INTO TrashBinInfo VALUES ('bin/sensor2', 0.2, 'lat lang');
 INSERT INTO TrashBinInfo VALUES ('bin/sensor3', 0.5, 'lat lang');
 INSERT INTO TrashBinInfo VALUES ('bin/sensor4', 1, 'lat lang');
+
+
+CREATE OR REPLACE FUNCTION notify_new_data() RETURNS TRIGGER AS $$ 
+DECLARE
+BEGIN
+  PERFORM pg_notify('watchers', 'SensorData');
+  RETURN new;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER on_recv_sensor
+AFTER INSERT
+ON SensorData
+FOR STATEMENT
+EXECUTE PROCEDURE notify_new_data()
